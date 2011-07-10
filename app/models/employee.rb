30e944fd88ef87710afe7f3cf474697d7d53a57a
Patrizio <patrick@rekeeb.com>
2011-07-10 17:48:14 +0200

    Added employee validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,5 +2,9 @@ class Employee < ActiveRecord::Base
   has_many :lunch_lists
   has_many :given_lunches, :through => :lunch_lists
 
+  validates :firstname, :presence => true
+  validates :lastname, :presence => true
+  validates :employee_id, :presence => true
+  validates_length_of :employee_id, :is => 5,
   
 end
