a7cb5fae389f090ee09500b43a8b8287cc3864dc
Patrizio <patrick@rekeeb.com>
2011-07-11 20:44:44 +0200

    Corrected wrong comma

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,11 +1,13 @@
 class Employee < ActiveRecord::Base
+  attr_accessible :firstname, :lastname, :employee_id
+  
   has_many :lunch_lists
   has_many :given_lunches, :through => :lunch_lists
 
   validates :firstname, :presence => true
   validates :lastname, :presence => true
   validates :employee_id, :presence => true
-  validates_length_of :employee_id, :is => 5,
+  validates_length_of :employee_id, :is => 5
   
   def fullname
     "#{firstname} #{lastname}"
