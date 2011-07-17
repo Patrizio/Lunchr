58c2dadb89df2e254f3f2d36b383f468a76266c1
Patrizio <patrick@rekeeb.com>
2011-07-17 14:16:37 +0200

    Added destroy dependancy

--- app/models/given_lunch.rb
+++ app/models/given_lunch.rb
@@ -1,5 +1,5 @@
 class GivenLunch < ActiveRecord::Base
-  has_many :lunch_lists
+  has_many :lunch_lists, :dependent => :destroy
   has_many :employees, :through => :lunch_lists
   
   validates :date_of_lunch, :presence => true
