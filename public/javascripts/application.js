7f53e5fa9f4d4bf15e8b2b17e6216d575fc0621c
Patrizio <patrick@rekeeb.com>
2011-07-17 16:24:10 +0200

    Show complete week with names and dates

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,38 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function()
-{
-	$('.turn-me-into-datepicker')
-		.datePicker({
-			inline:true, 
-			dateFormat: 'yy-mm-dd',
-			startDate: new Date(2011, 1 - 1, 1),
-			endDate:new Date(2011, 12 - 1, 31),
-		})
-		.bind(
-			'dateSelected',
-			function(e, selectedDate, $td)
-			{
-				$('#given_lunch_date_of_lunch').val(selectedDate.asString());
-			}
-		);
+
+
+$(document).ready(function() {
+  	$('input').change(function () {
+	    if ($(this).attr("checked")) {
+	        //do the stuff that you would do when 'checked'
+			$(this).parent().css('background-color', 'lightyellow');
+	        return;
+	    }
+	    //Here do the stuff you want to do when 'unchecked'
+		$(this).parent().css('background-color', 'lightgrey');
+	});
+	
+	$(function()
+	{
+		$('.turn-me-into-datepicker')
+			.datePicker({
+				inline:true, 
+				dateFormat: 'yy-mm-dd',
+				startDate: new Date(2011, 1 - 1, 1),
+				endDate:new Date(2011, 12 - 1, 31),
+			})
+			.bind(
+				'dateSelected',
+				function(e, selectedDate, $td)
+				{
+					$('#given_lunch_date_of_lunch').val(selectedDate.asString());
+				}
+			);
+	});
 });
+
+
+
+
